93be26bc61621541c3b24732c630166daf026830
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-08-26 20:33:33 -0300

    feat: dark theme on application

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
-import { formatBytes } from '@/utils/format-bytes'
 
 import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { Button } from '@/components/Button'
 import { FileItem } from './FileItem'
 
 export function FileList() {
@@ -15,7 +12,14 @@ export function FileList() {
   return (
     <div className="mt-4 space-y-3" ref={parent}>
       {files.map((file) => {
-        return <FileItem name={file.name} size={file.size} key={file.name} />
+        return (
+          <FileItem
+            state="complete"
+            name={file.name}
+            size={file.size}
+            key={file.name}
+          />
+        )
       })}
     </div>
   )
